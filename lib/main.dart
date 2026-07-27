a0677674f810a48071241c0effb43db2533b3943
Imene <imane.bendjemaa@ensia.edu.dz>
2026-07-27 23:17:31 +0100

    initialize the firebase in the main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,24 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:readly/core/routing/app_router.dart';
+import 'package:readly/firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  try {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    print("Firebase initialized!");
+  } on FirebaseException catch (e) {
+    debugPrint('Firebase initialization failed: ${e.message}');
+    rethrow;
+  } catch (e) {
+    debugPrint('Unexpected error during Firebase initialization: $e');
+    rethrow;
+  }
+
   runApp(const MainApp());
 }
 
@@ -12,9 +29,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       routerConfig: appRouter,
-      theme: ThemeData(
-        fontFamily: "Baloo2",
-      ),
+      theme: ThemeData(fontFamily: "Baloo2"),
     );
   }
 }
